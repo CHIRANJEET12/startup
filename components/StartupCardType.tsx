8b62b6b0661a98e390d1cb445428bd3adaa4c169
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2025-06-06 13:26:59 +0530

    Sanity work done and publishing some users

--- components/StartupCardType.tsx
+++ components/StartupCardType.tsx
@@ -3,6 +3,7 @@ import { formatDate } from "@/lib/utils"
 import { EyeIcon } from "lucide-react"
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from "@/components/ui/button";
 
 
 type posttype = {
@@ -20,30 +21,44 @@ type posttype = {
 export const StartupCardType = ({ post }: { post: posttype }) => {
     return (
         <li className='startup-card group'>
-            <div className='flex-between'>
-                <p className='startup-card_date'>
+            <div className='flex-between '>
+                <div className='startup-card_date'>
                     {formatDate(post._createdAt)}
-                </p>
-                <div className='flex gap-1.5'>
-                    <EyeIcon className="size-9 text-pink-500" />
-                    <span className='text-2xl font-bold'>
-                        {post.views}
-                    </span>
+                    <div className='flex gap-1.5'>
+                        <EyeIcon className="size-5 text-pink-500" />
+                        <span className='text-sm font-bold'>
+                            {post.views}
+                        </span>
+                    </div>
                 </div>
-                <div className='flex-between gap-5'>
+                <br />
+
+                <div className='flex gap-5'>
                     <div className='flex-1'>
                         <Link href={`/user/${post.author?._id}`}>
                             <p className='text-3xl font-bold line-clamp-1'>{post.author?.name}</p>
                         </Link>
-                        <Link href={`/startup/${post._id}`}>
-                            <h3 className='text-2xl font-semibold line-clamp-1'>
-                                {post.description}
-                                <Image src={post.image} className='startup-card_img' alt='Image' width={800} height={600} />
-                            </h3>
-                        </Link>
-                        {/* <Link href={`/user/${post.author?._id}`}>
-                    </Link> */}
                     </div>
+                    <Link href={`/user/${post.author?._id}`}>
+                        <Image src="https://placehold.co/48x48" alt='placeholder' width={48} height={48} className='rounded-full' />
+                    </Link>
+                </div>
+                <Link href={`/startup/${post._id}`}>
+                    <h3 className='text-2xl font-semibold line-clamp-1'>
+                        {post.description}
+                        <Image src={post.image} className='startup-card_img' alt='Image' width={800} height={600} />
+                    </h3>
+                </Link>
+
+                <div className='flex justify-between items-center gap-3 mt-5'>
+                    <Link href={`/?query=${post.category.toLowerCase()}`}>
+                        <p className='text-16 font-medium'>{post.category}</p>
+                    </Link>
+                    <Button className="startup-card-btn" asChild>
+                        <Link href={`/startup/${post._id}`}>
+                            Details
+                        </Link>
+                    </Button>
                 </div>
             </div>
         </li>
